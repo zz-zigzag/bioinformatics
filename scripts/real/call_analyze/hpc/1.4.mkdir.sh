cac9fcacd8ad849d12829f76bd3a2669edee4c21
zz_zigzag <zz_zigzag@outlook.com>
2016-06-29 11:01:56 +0800

    add feature

--- scripts/real/call_analyze/hpc/1.4.mkdir.sh
+++ scripts/real/call_analyze/hpc/1.4.mkdir.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+mkdir -p bsub_out
+mv *.out bsub_out/
+
+mkdir -p list used_sh feature
+
+mkdir -p bam/bam_cfg
+mv *.cfg bam/bam_cfg
+mv *.bam *.bai *.bas bam/
+
 mkdir -p raw_callset/pindel
-mv out.pindel* raw_callset/pindel && cd raw_callset/pindel && ../../1.0.simplify_pindel_callset.sh && tar czvf res.pindel.tar.gz out* && rm out* && cd -
+mv out.pindel* raw_callset/pindel && cd raw_callset/pindel && . ../../1.0.simplify_pindel_callset.sh && tar czvf res.pindel.tar.gz out* && rm out* && cd -
 
 mkdir -p raw_callset/svseq
-mv out.svseq* raw_callset/svseq && cd raw_callset/svseq && ../../1.0.simplify_svseq_callset.sh && tar czvf res.svseq.tar.gz out* && rm out* && cd -
+mv out.svseq* raw_callset/svseq && cd raw_callset/svseq && . ../../1.1.simplify_svseq_callset.sh && tar czvf res.svseq.tar.gz out* && rm out* && cd -
 
 mkdir -p raw_callset/breakdancer
 cp out.breakdancer* raw_callset/breakdancer
